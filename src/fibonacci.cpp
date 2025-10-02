81d1592120a36a8a2cb7e42308e617224f330e5e
Lucas Ergh <LucasErgh@gmail.com>
2025-10-02 17:30:45 -0400

    First iteration

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "fibonacci.hpp"
+#include <iostream>
 
 namespace fibonacci {
 
@@ -13,13 +14,50 @@ void fibonacciRacer(std::array<uint64_t, MAX_64_BIT_FIBONACCI_INDEX + 1>& result
     /**
      * @todo Implement the fibonacciRacer function declared in include/fibonacci.hpp.
      */
+
+    int index = 0;
+    uint64_t cur = 1;
+    uint64_t prev = 1;
+    uint64_t prevPrev = 0;
+
+    while (index <= end) {
+        if (index >= start && index <= end) {
+            results[index - start] = cur;
+            index++;
+        }
+
+        cur = prev + prevPrev;
+
+        prevPrev = prev;
+        prev = cur;
+    }
 }
 
 uint64_t fibonacci(int n) {
     /**
      * @todo Implement the fibonacci function declared in include/fibonacci.hpp.
      */
-    return -1;
+
+    int index = 1;
+
+    uint64_t cur = 1;
+    uint64_t prev = 1;
+    uint64_t prevPrev = 0;
+
+    while (index <= n) {
+    // std::cout << "Index: " << index << "    " << cur << '\n';
+        if (index == n) {
+            return cur;
+        }
+        ++index;
+
+        cur = prev + prevPrev;
+
+        prevPrev = prev;
+        prev = cur;
+
+    }
+        return -1;
 }
 
 } // namespace fibonacci
